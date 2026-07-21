a020c5f416d97aef9567ef47818e0c10c389a456
MohiuddinCo22 <161435524+MohiuddinCo22@users.noreply.github.com>
2026-07-21 15:51:00 +0600

    Update auth.js

--- JS/auth.js
+++ JS/auth.js
@@ -1,1 +1,30 @@
+import { auth } from "./firebase.js";
 
+import {
+signInWithEmailAndPassword
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const loginBtn=document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click",()=>{
+
+const email=document.getElementById("email").value;
+
+const password=document.getElementById("password").value;
+
+signInWithEmailAndPassword(auth,email,password)
+
+.then(()=>{
+
+window.location.href="dashboard.html";
+
+})
+
+.catch((error)=>{
+
+document.getElementById("error").innerHTML=error.message;
+
+});
+
+});
