1f56744a14e255b815b92e19389a00e5db945561
MohiuddinCo22 <161435524+MohiuddinCo22@users.noreply.github.com>
2026-07-21 17:05:44 +0600

    Update dashboard.js

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,1 +1,57 @@
+import { auth } from "./firebase.js";
+import { generateKey } from "./keyGenerator.js";
 
+import {
+onAuthStateChanged,
+signOut
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+
+// Authentication Guard
+
+onAuthStateChanged(auth,(user)=>{
+
+if(user){
+
+console.log("Logged in:",user.email);
+
+}else{
+
+window.location.href="login.html";
+
+}
+
+});
+
+
+
+// Generate Button
+
+const generateBtn = document.getElementById("generateBtn");
+
+
+generateBtn.addEventListener("click",()=>{
+
+generateKey();
+
+});
+
+
+
+// Logout
+
+const logoutBtn=document.getElementById("logoutBtn");
+
+
+logoutBtn.addEventListener("click",()=>{
+
+
+signOut(auth)
+.then(()=>{
+
+window.location.href="login.html";
+
+});
+
+
+});
