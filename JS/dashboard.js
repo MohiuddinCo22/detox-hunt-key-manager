696d539c3d681e42a4420151a53412516ed7bf81
MohiuddinCo22 <161435524+MohiuddinCo22@users.noreply.github.com>
2026-07-21 17:28:52 +0600

    Update dashboard.js

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,10 +1,17 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 import { generateKey } from "./keyGenerator.js";
 
 import {
 onAuthStateChanged,
 signOut
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+collection,
+onSnapshot,
+query,
+orderBy
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 
 // Authentication Guard
@@ -14,6 +21,8 @@ onAuthStateChanged(auth,(user)=>{
 if(user){
 
 console.log("Logged in:",user.email);
+
+loadKeys();
 
 }else{
 
@@ -25,9 +34,10 @@ window.location.href="login.html";
 
 
 
+
 // Generate Button
 
-const generateBtn = document.getElementById("generateBtn");
+const generateBtn=document.getElementById("generateBtn");
 
 
 generateBtn.addEventListener("click",()=>{
@@ -38,13 +48,13 @@ generateKey();
 
 
 
+
 // Logout
 
 const logoutBtn=document.getElementById("logoutBtn");
 
 
 logoutBtn.addEventListener("click",()=>{
-
 
 signOut(auth)
 .then(()=>{
@@ -53,5 +63,83 @@ window.location.href="login.html";
 
 });
 
+});
+
+
+
+
+
+// Load Keys From Firestore
+
+
+function loadKeys(){
+
+
+const keyTable=document.getElementById("keyList");
+
+
+const q=query(
+collection(db,"keys"),
+orderBy("createdAt","desc")
+);
+
+
+
+onSnapshot(q,(snapshot)=>{
+
+
+keyTable.innerHTML="";
+
+
+let sl=1;
+
+
+
+snapshot.forEach((doc)=>{
+
+
+const data=doc.data();
+
+
+
+keyTable.innerHTML += `
+
+<tr>
+
+<td>${sl}</td>
+
+<td>${data.userId}</td>
+
+<td>${data.key}</td>
+
+<td>৳${data.price}</td>
+
+<td>${data.status}</td>
+
+<td>
+<button>
+Copy
+</button>
+</td>
+
+</tr>
+
+`;
+
+
+
+sl++;
+
 
 });
+
+
+
+document.getElementById("totalKeys").innerText=snapshot.size;
+
+
+
+});
+
+
+}
